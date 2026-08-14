0bad97490cd5942b1dd5dafae0aa68cc2376442b
JuaumQxC <joaoaugusto3235@gmail.com>
2026-08-14 02:52:03 +0000

    exe 1 e 2

--- serverTeste.js
+++ serverTeste.js
@@ -1,10 +1,9 @@
-// exercício 1
-const http = require('node:http');
-
+// exercício 1 e 2
 const PORTA = 3000;
+datareq = new Date().toISOString()
 
 const server = http.createServer((req, res) => {
-    console.log(`Requisição recebida: ${req.method} ${req.url}`);
+    console.log(`Requisição recebida: ${req.method} ${req.url} em ${datareq}`);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');
     res.end('Recurso criado\n');

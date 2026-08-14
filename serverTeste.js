49130023a3181042ea3a24bcf0b8810d5353a3c0
JuaumQxC <joaoaugusto3235@gmail.com>
2026-08-14 03:13:47 +0000

    exe 3 e 4

--- serverTeste.js
+++ serverTeste.js
@@ -13,3 +13,21 @@ const server = http.createServer((req, res) => {
 server.listen(PORTA, () => {
     console.log(`Servidor escutando em http://localhost:${PORTA}`);
 });
+
+// exercício 3
+const PORTA = 3000;
+
+const server = http.createServer((req, res) => {
+    console.log(`Requisição recebida: ${req.method} ${req.url}`);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    res.end(`${JSON.stringify({ status: "ok" })}`);
+});
+
+server.listen(PORTA, () => {
+    console.log(`Servidor escutando em http://localhost:${PORTA}`);
+});
+
+// exercício 4:
+// acreditava que, ao tirar o res.end() do codigo acima iria dar alguma mensagem de erro,
+// mas fui surpreendido em ver que restava apenas uma página em branco no servidor.
